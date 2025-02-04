c3ca8c7a109d3f8f10ea3053c7d92c94a27ae53a
Jannic <jannic.marcon@hotmail.com>
2025-02-04 21:17:54 +0100

    create activeTrade

--- Sources/backtestActor.swift
+++ Sources/backtestActor.swift
@@ -1,4 +1,4 @@
-import Foundation
+import Foundatio
 
 /*
     This Actor is the main for the backtesting. it works as its own program
@@ -30,13 +30,15 @@ actor BacktestActor{
         // 1. Get high and low for up and down trend
         // 2. Reset high and low at the right times
         // 3. Calculate GP when the second pivot point is generated
-
+        // 4. Create a "activeTrade" with all the trade rules (entry, exit levels)
 
 
         var candleHigh: IndicatorCandle?
         var candleLow: IndicatorCandle?
 
         var lastCrossIndex = 0
+
+        var activeTrade: Trade?
 
         for i in 1..<candles.count{
             let currCandle = candles[i]
@@ -73,11 +75,20 @@ actor BacktestActor{
 
 
             
-            if let candleLow, let candleHigh{
-                let candleZero = isBullTrend ? candleHigh.high : candleLow.low
-                let candleOne = isBullTrend ? candleLow.low : candleHigh.high
+            if let low = candleLow, let high = candleHigh{
+                let candleZero = isBullTrend ? high.high : low.low
+                let candleOne = isBullTrend ? low.low : high.high
 
                 let fibs = getFibonacci(zero: candleZero, one: candleOne)
+
+                activeTrade = Trade(entry1: fibs[0.618]!, entry2: fibs[0.674]!, entry3: fibs[0.73]!, entry4: fibs[0.786]!,
+                                    isEntry1: false, isEntry2: false, isEntry3: false, isEntry4: false,
+                                    stopLoss: fibs[1]!, isStopLoss: false,
+                                    stopLossProfit: fibs[0.58]!, isStopLossProfit: false,
+                                    takeProfit1: fibs[0.382]!, takeProfit2: fibs[0]!, takeProfitCross: nil,
+                                    isTakeProfit1: false, isTakeProfit2: false)
+                candleHigh = nil
+                candleLow = nil
             }
 
 
@@ -97,7 +108,9 @@ actor BacktestActor{
                             0.5: 0.0,
                             0.58: 0.0,
                             0.618: 0.0,
-                            0.75: 0.0,
+                            0.674: 0.0,
+                            0.73: 0.0,
+                            0.786: 0.0,
                             0.886: 0.0,
                             1: one]
 
@@ -105,7 +118,7 @@ actor BacktestActor{
 
         for key in fibSequence.keys{
             let diffFactor = key * difference
-            fibSequence[key] = zero>one ? zero - diffFactor : zero + diffFactor
+            fibSequence[key] = zero>one ? ((zero - diffFactor) * 100).rounded() / 100 : ((zero + diffFactor) * 100).rounded() / 100
         }
 
         return fibSequence
